3ff00abe67f047455d4d339b09e4dcc2c4357d50
leon <leon@leons-MacBook-Air.local>
2023-10-01 16:06:58 +0100

    feat: remove a [path] source type

--- ForensicCaseManager/Functions/Public/Parser/forensic_artifact.py
+++ ForensicCaseManager/Functions/Public/Parser/forensic_artifact.py
@@ -10,13 +10,12 @@ from dissect.target.filesystem import Filesystem
 from path_finder import ARTIFACT_PATH
 from util.ts import TimeStamp
 
-SOURCE_TYPE_PATH = "Path"
 SOURCE_TYPE_LOCAL = "Local"
 SOURCE_TYPE_CONTAINER = "Container"
 
+
 @dataclass
 class Source:
-    _path: str = None
     _local: bool = False
     _container: str = None
     source: Union[str, Target] = field(init=False)
@@ -24,12 +23,7 @@ class Source:
     type: str = field(init=False)
 
     def __post_init__(self):
-        if self._path:
-            self.source = self._path
-            self.source_path = self._path
-            self.type = SOURCE_TYPE_PATH
-            self._target = ""  #
-        elif self._local:
+        if self._local:
             self.source = Target.open("local")
             self.source_path = "Local"
             self.type = SOURCE_TYPE_LOCAL
@@ -41,7 +35,8 @@ class Source:
             self._target = self.source  #
         else:
             raise ValueError(f"Error :(")
-                
+
+
 @dataclass(kw_only=True)
 class ForensicArtifact:
     src: Source
@@ -54,7 +49,7 @@ class ForensicArtifact:
     def __post_init__(self):
         self.directory, self.entry = ARTIFACT_PATH.get(self.artifact)
         self._target = self.src._target  #
-        
+
     @property
     def ts(self) -> TimeStamp:
         try:
@@ -64,38 +59,34 @@ class ForensicArtifact:
         return TimeStamp(tzinfo=timezone(bias))
 
     def _iter_filesystem(self, type: str = "ntfs") -> Generator[Filesystem, None, None]:
-        if self.src.type == SOURCE_TYPE_LOCAL or self.src.type == SOURCE_TYPE_CONTAINER:
-            yield from (
-                fs for fs in self.src.source.filesystems
-                if fs.__fstype__ == type
-            )
+        yield from (fs for fs in self.src.source.filesystems if fs.__fstype__ == type)
 
     def _iter_directory(self) -> Generator[Path, None, None]:
-        if self.src.type == SOURCE_TYPE_LOCAL or self.src.type == SOURCE_TYPE_CONTAINER:
-            for dir in self.directory:
-                if dir.startswith("%ROOT%"):
-                    dir = Path(dir.replace("%ROOT%", ""))
-                    yield from (
-                        root.joinpath(dir)
-                        for root in self.src.source.fs.path("/").iterdir()
-                        if root.joinpath(dir).parts[1] != "sysvol" and root.joinpath(dir).exists()
-                    )
-                elif dir.startswith("%USER%"):
-                    dir = Path(dir.replace("%USER%", ""))
-                    yield from (
-                        user_details.home_path.joinpath(dir)
-                        for user_details in self.src.source.user_details.all_with_home()
-                        if user_details.home_path.joinpath(dir).exists()
-                    )
-        elif self.src.type == SOURCE_TYPE_PATH:
-            yield Path(self.src.source)
+        for dir in self.directory:
+            if dir.startswith("%ROOT%"):
+                dir = Path(dir.replace("%ROOT%", ""))
+                yield from (
+                    root.joinpath(dir)
+                    for root in self.src.source.fs.path("/").iterdir()
+                    if root.joinpath(dir).parts[1] != "sysvol"
+                    and root.joinpath(dir).exists()
+                )
+            elif dir.startswith("%USER%"):
+                dir = Path(dir.replace("%USER%", ""))
+                yield from (
+                    user_details.home_path.joinpath(dir)
+                    for user_details in self.src.source.user_details.all_with_home()
+                    if user_details.home_path.joinpath(dir).exists()
+                )
 
-    def _iter_entry(self, name: str = None, recurse: bool = False) -> Generator[Path, None, None]:
+    def _iter_entry(
+        self, name: str = None, recurse: bool = False
+    ) -> Generator[Path, None, None]:
         if name == None:
             entry = self.entry
         else:
             entry = name
-            
+
         for dir in self._iter_directory():
             if dir.is_dir():
                 if recurse == True:
@@ -124,7 +115,7 @@ class ForensicArtifact:
     def export(self, output_dir: Path, current_time: str = None) -> list[dict]:
         if not output_dir.exists():
             os.makedirs(output_dir, 0o777)
-            
+
         if current_time == None:
             current_time = datetime.now().strftime("%Y%m%dT%H%M%S")
 
@@ -132,14 +123,16 @@ class ForensicArtifact:
         for name, data in self.result.items():
             result = "[" + ",\n".join(data) + "]"
             output_path = output_dir / f"{name}_{current_time}.json"
-            with open(output_path, 'a+', encoding='utf-8') as f:
+            with open(output_path, "a+", encoding="utf-8") as f:
                 f.write(result)
 
             result_file = {
                 "category": self.category,
                 "artifact": self.artifact,
                 "record": name,
-                "result": output_path
+                "result": output_path,
             }
-            result_files.append(json.dumps(result_file, indent=2, default=str, ensure_ascii=False))
+            result_files.append(
+                json.dumps(result_file, indent=2, default=str, ensure_ascii=False)
+            )
         return result_files
