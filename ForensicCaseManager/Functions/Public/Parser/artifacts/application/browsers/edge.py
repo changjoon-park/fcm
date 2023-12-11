f8f0ff412bb85388ef73253b09e16bf6d1fc6036
cj.park <sisyphus9402@gmail.com>
2023-12-11 15:07:23 +0900

    feat: add artifact (edge) for Edge browser artifacts   - Create artifacts schema   - Define edge artifacts

--- ForensicCaseManager/Functions/Public/Parser/artifacts/application/browsers/edge.py
+++ ForensicCaseManager/Functions/Public/Parser/artifacts/application/browsers/edge.py
@@ -2,51 +2,62 @@ import json
 
 from forensic_artifact import Source
 from artifacts.application.browsers.browser import ChromiumBrowser
+from settings import (
+    ART_EDGE,
+    RSLT_EDGE_HISTORY,
+    RSLT_EDGE_DOWNLOADS,
+    RSLT_EDGE_KEYWORD_SEARCH_TERMS,
+    RSLT_EDGE_AUTOFILL,
+    RSLT_EDGE_LOGIN_DATA,
+    RSLT_EDGE_BOOKMARKS,
+)
 
 
 class Edge(ChromiumBrowser):
-
     def __init__(self, src: Source, artifact: str, category: str):
-        super().__init__(
-            src=src,
-            artifact=artifact,
-            category=category
-        )
-        
+        super().__init__(src=src, artifact=artifact, category=category)
+
     @property
     def browser_type(self) -> str:
-        return "edge"
-        
+        return ART_EDGE
+
     def parse(self, descending: bool = False) -> None:
-        history = sorted([
-                json.dumps(record._packdict(), indent=2, default=str, ensure_ascii=False)
-                for record in self.history()], reverse=descending)
+        history = sorted(
+            [record for record in self.history()],
+            key=lambda record: record["ts"],
+            reverse=descending,
+        )
+        downloads = sorted(
+            [record for record in self.downloads()],
+            key=lambda record: record["ts_start"],
+            reverse=descending,
+        )
+        keyword_search_terms = sorted(
+            [record for record in self.keyword_search_terms()],
+            key=lambda record: record["ts"],
+            reverse=descending,
+        )
+        autofill = sorted(
+            [record for record in self.autofill()],
+            key=lambda record: record["ts_created"],
+            reverse=descending,
+        )
+        login_data = sorted(
+            [record for record in self.login_data()],
+            key=lambda record: record["ts_created"],
+            reverse=descending,
+        )
+        bookmarks = sorted(
+            [record for record in self.bookmarks()],
+            key=lambda record: record["ts_added"],
+            reverse=descending,
+        )
 
-        downloads = sorted([
-                json.dumps(record._packdict(), indent=2, default=str, ensure_ascii=False)
-                for record in self.downloads()], reverse=descending)
-        
-        keyword_search_terms = sorted([
-                json.dumps(record._packdict(), indent=2, default=str, ensure_ascii=False)
-                for record in self.keyword_search_terms()], reverse=descending)
-
-        autofill = sorted([
-                json.dumps(record._packdict(), indent=2, default=str, ensure_ascii=False)
-                for record in self.autofill()], reverse=descending)
-
-        login_data = sorted([
-                json.dumps(record._packdict(), indent=2, default=str, ensure_ascii=False)
-                for record in self.login_data()], reverse=descending)
-        
-        bookmarks = sorted([
-                json.dumps(record._packdict(), indent=2, default=str, ensure_ascii=False)
-                for record in self.bookmarks()], reverse=descending)
-        
         self.result = {
-            "edge_history": history,
-            "edge_downloads": downloads,
-            "edge_keyword_search_terms": keyword_search_terms,
-            "edge_autofill": autofill,
-            "edge_login_data": login_data,
-            "edge_bookmarks": bookmarks,
+            RSLT_EDGE_HISTORY: history,
+            RSLT_EDGE_DOWNLOADS: downloads,
+            RSLT_EDGE_KEYWORD_SEARCH_TERMS: keyword_search_terms,
+            RSLT_EDGE_AUTOFILL: autofill,
+            RSLT_EDGE_LOGIN_DATA: login_data,
+            RSLT_EDGE_BOOKMARKS: bookmarks,
         }
